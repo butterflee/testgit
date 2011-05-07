f779dc64d0af6a5f628df6a0c110053106dba7af
jxd <jxd@localhost>
2011-05-07 15:07:45 -0400

    rename a folder, then add same folder and a new folder in branche a

---
+++ src/com/bar/Bar1.java
@@ -0,0 +1,13 @@
+package com.bar;
+
+public class Bar1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/foo/Hello.java
@@ -0,0 +1,5 @@
+package com.foo;
+
+public class Hello {
+
+}
