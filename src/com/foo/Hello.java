21483a08c522934ff33d233979dad4a2c22c4fd5
jxd <jxd@localhost>
2011-05-07 14:53:01 -0400

    branch a commit

--- src/com/foo/Hello.java
+++ src/com/foo/Hello.java
@@ -8,6 +8,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.out.println("world!!!!");
+		
 
 	}
 
--- src/com/foo/World.java
+++ src/com/foo/World.java
@@ -2,5 +2,8 @@ package com.foo;
 
 public class World {
 
-	
+	public static void main(String args[])
+	{
+		System.out.println("world.");
+	}
 }
